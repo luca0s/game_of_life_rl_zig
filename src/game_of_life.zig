e50e8b806fc5954cdd318c6f89f3a87e7a7e3eb3
luca0s <luca@schluer.eu>
2025-03-31 01:36:28 +0000

    used a more optimized algorithm and added the ability to paint in new cells

--- src/game_of_life.zig
+++ src/game_of_life.zig
@@ -1,111 +1,99 @@
 const std = @import("std");
 
+pub const Point = struct { x: i32, y: i32 };
+
 pub const GameOfLife = struct {
-    cells: [][]bool,
+    cells: std.AutoHashMap(Point, bool),
 
-    pub fn init(height: usize, width: usize) !GameOfLife {
+    pub fn init() !GameOfLife {
         const allocator = std.heap.page_allocator;
-        const new_cells = try allocator.alloc([]bool, height);
-
-        for (0..new_cells.len) |i| {
-            new_cells[i] = try allocator.alloc(bool, width);
-        }
-
         return .{
-            .cells = new_cells,
+            .cells = std.AutoHashMap(Point, bool).init(allocator),
         };
     }
 
     pub fn update(self: *GameOfLife) !void {
         const allocator = std.heap.page_allocator;
-        var new_cells = try allocator.alloc([]bool, self.cells.len);
+        var new_cells = std.AutoHashMap(Point, bool).init(allocator);
+        var iter = self.cells.iterator();
 
-        for (0..new_cells.len) |i| {
-            new_cells[i] = try allocator.alloc(bool, self.cells[i].len);
-        }
+        while (iter.next()) |entry| {
+            const num_neighbours = get_num_of_neighbours(self.cells, entry.key_ptr.*);
 
-        for (0..self.cells.len) |i| {
-            for (0..self.cells[i].len) |j| {
-                const num_of_neighbours = self.get_num_of_neighbours(i, j);
+            if (entry.value_ptr.*) {
+                const alive = switch (num_neighbours) {
+                    0...1 => false,
+                    2...3 => true,
+                    else => false,
+                };
 
-                if (self.cells[i][j]) {
-                    new_cells[i][j] = switch (num_of_neighbours) {
-                        0...1 => false,
-                        2...3 => true,
-                        else => false,
-                    };
-                } else {
-                    if (num_of_neighbours == 3) {
-                        new_cells[i][j] = true;
-                    }
+                if (alive or num_neighbours > 0) {
+                    try new_cells.put(Point{ .x = entry.key_ptr.*.x, .y = entry.key_ptr.*.y }, alive);
+                    try add_neighbours(&new_cells, entry.key_ptr.*);
+                }
+            } else {
+                const alive = switch (num_neighbours) {
+                    3 => true,
+                    else => false,
+                };
+
+                if (alive or num_neighbours > 3) {
+                    try new_cells.put(Point{ .x = entry.key_ptr.*.x, .y = entry.key_ptr.*.y }, alive);
+                    try add_neighbours(&new_cells, entry.key_ptr.*);
                 }
             }
         }
 
-        for (self.cells) |row| {
-            allocator.free(row);
-        }
-        allocator.free(self.cells);
-
+        self.cells.deinit();
         self.cells = new_cells;
     }
 
-    pub fn set_cells(self: GameOfLife, positions: []const [2]usize) void {
-        for (positions) |pos| {
-            if (pos.len != 2) {
-                return;
+    pub fn set_cells(self: *GameOfLife, points: []const Point) !void {
+        for (points) |point| {
+            std.debug.print("Setting cell: x={} y={}\n", .{ point.x, point.y });
+            if (self.cells.getEntry(point)) |entry| {
+                if (entry.value_ptr.*) {
+                    continue;
+                }
             }
-
-            if (!(pos[0] < self.cells.len and pos[1] < self.cells[pos[0]].len)) {
-                return;
-            }
-
-            self.cells[pos[0]][pos[1]] = true;
+            std.debug.print("Cell set", .{});
+            try self.cells.put(point, true);
+            try add_neighbours(&self.cells, point);
         }
     }
 
     pub fn print(self: GameOfLife) void {
-        for (self.cells) |row| {
-            for (row) |cell| {
-                if (cell) {
-                    std.debug.print("1", .{});
-                } else {
-                    std.debug.print("0", .{});
-                }
-            }
-            std.debug.print("\n", .{});
+        var iter = self.cells.iterator();
+        while (iter.next()) |entry| {
+            const point = entry.key_ptr.*;
+            const alive = entry.value_ptr.*;
+            std.debug.print("Cell: x={} y={} alive={}\n", .{ point.x, point.y, alive });
         }
     }
+};
 
-    pub fn get_num_of_neighbours(self: GameOfLife, i: usize, j: usize) u8 {
-        const dirs = [8][2]i8{ .{ -1, -1 }, .{ -1, 0 }, .{ -1, 1 }, .{ 0, -1 }, .{ 0, 1 }, .{ 1, -1 }, .{ 1, 0 }, .{ 1, 1 } };
-        var sum: u8 = 0;
+fn add_neighbours(cells: *std.AutoHashMap(Point, bool), point: Point) !void {
+    const dirs = [8][2]i32{ .{ -1, -1 }, .{ -1, 0 }, .{ -1, 1 }, .{ 0, -1 }, .{ 0, 1 }, .{ 1, -1 }, .{ 1, 0 }, .{ 1, 1 } };
 
-        for (dirs) |dir| {
-            //perform bounds check
-            if ((i == 0 and dir[0] == -1) or (j == 0 and dir[1] == -1) or (i == self.cells.len - 1 and dir[0] == 1) or (j == self.cells[j].len - 1 and dir[1] == 1)) {
-                continue;
-            }
+    for (dirs) |dir| {
+        const new_neighbour = Point{ .x = point.x + dir[0], .y = point.y + dir[1] };
+        if (!cells.contains(new_neighbour)) {
+            try cells.put(new_neighbour, false);
+        }
+    }
+}
 
-            const off_i: usize = switch (dir[0]) {
-                -1 => i - 1,
-                0 => i,
-                1 => i + 1,
-                else => 0,
-            };
+fn get_num_of_neighbours(cells: std.AutoHashMap(Point, bool), point: Point) u8 {
+    const dirs = [8][2]i32{ .{ -1, -1 }, .{ -1, 0 }, .{ -1, 1 }, .{ 0, -1 }, .{ 0, 1 }, .{ 1, -1 }, .{ 1, 0 }, .{ 1, 1 } };
+    var sum: u8 = 0;
 
-            const off_j: usize = switch (dir[1]) {
-                -1 => j - 1,
-                0 => j,
-                1 => j + 1,
-                else => 0,
-            };
-
-            if (self.cells[off_i][off_j]) {
+    for (dirs) |dir| {
+        if (cells.get(Point{ .x = point.x + dir[0], .y = point.y + dir[1] })) |alive| {
+            if (alive) {
                 sum += 1;
             }
         }
-
-        return sum;
     }
-};
+
+    return sum;
+}
--- src/main.zig
+++ src/main.zig
@@ -1,39 +1,87 @@
 const std = @import("std");
 const rl = @import("raylib");
 const gameOfLife = @import("game_of_life.zig").GameOfLife;
+const Point = @import("game_of_life.zig").Point;
+
+const UiState = enum {
+    DrawNewCells,
+    RunnigSim,
+};
+
+const GameState = struct {
+    cellsize: i32,
+    grid_w: i32,
+    grid_h: i32,
+    game: gameOfLife,
+    ui_state: UiState,
+};
+
+var state: GameState = GameState{
+    .cellsize = 5,
+    .grid_w = 200,
+    .grid_h = 200,
+    .game = undefined,
+    .ui_state = UiState.RunnigSim,
+};
 
 pub fn main() !void {
-    const cellsize: i32 = 2;
-    const grid_w: i32 = 500;
-    const grid_h: i32 = 500;
-
-    const window_w: i32 = cellsize * grid_w;
-    const window_h: i32 = cellsize * grid_h;
+    const window_w: i32 = state.cellsize * state.grid_w;
+    const window_h: i32 = state.cellsize * state.grid_h;
 
     rl.initWindow(window_w, window_h, "Game of Life");
-    rl.setTargetFPS(200);
+    rl.setTargetFPS(30);
 
-    //init game of life
-    var game = try gameOfLife.init(window_h, window_w);
-    const starting_cells = [_][2]usize{ [2]usize{ 20, 20 }, [2]usize{ 21, 21 }, [2]usize{ 21, 22 }, [2]usize{ 20, 22 }, [2]usize{ 19, 22 } };
+    state.game = try gameOfLife.init();
+    const starting_cells = [_]Point{
+        Point{ .x = 20, .y = 20 },
+        Point{ .x = 21, .y = 21 },
+        Point{ .x = 21, .y = 22 },
+        Point{ .x = 20, .y = 22 },
+        Point{ .x = 19, .y = 22 },
+    };
 
-    game.set_cells(starting_cells[0..]);
+    try state.game.set_cells(starting_cells[0..]);
+    state.game.print();
 
     while (!rl.windowShouldClose()) {
         rl.beginDrawing();
         defer rl.endDrawing();
 
-        for (0..game.cells.len) |i| {
-            for (0..game.cells[i].len) |j| {
-                if (game.cells[i][j]) {
-                    const i_cast: i32 = @intCast(i);
-                    const j_cast: i32 = @intCast(j);
-                    rl.drawRectangle(i_cast * cellsize, j_cast * cellsize, cellsize, cellsize, .gray);
-                }
+        if (rl.isKeyPressed(rl.KeyboardKey.space)) {
+            state.ui_state = UiState.DrawNewCells;
+        } else if (rl.isKeyPressed(.enter)) {
+            state.ui_state = UiState.RunnigSim;
+        }
+
+        var iter = state.game.cells.iterator();
+        while (iter.next()) |entry| {
+            if (entry.value_ptr.*) {
+                const x = entry.key_ptr.x;
+                const y = entry.key_ptr.y;
+                rl.drawRectangle(x * state.cellsize, y * state.cellsize, state.cellsize, state.cellsize, .gray);
             }
         }
 
-        try game.update();
+        switch (state.ui_state) {
+            UiState.DrawNewCells => try draw_cells_ui(),
+            UiState.RunnigSim => try state.game.update(),
+        }
+
+        rl.drawFPS(10, 10);
         rl.clearBackground(.white);
+    }
+}
+
+fn draw_cells_ui() !void {
+    if (rl.isMouseButtonDown(.left)) {
+        const mouse_pos = rl.getMousePosition();
+
+        const mouse_x = mouse_pos.x / @as(f32, @floatFromInt(state.cellsize));
+        const mouse_y = mouse_pos.y / @as(f32, @floatFromInt(state.cellsize));
+
+        try state.game.set_cells(&[_]Point{Point{
+            .x = @intFromFloat(mouse_x),
+            .y = @intFromFloat(mouse_y),
+        }});
     }
 }
